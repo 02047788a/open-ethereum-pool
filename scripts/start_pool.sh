bc57effc36649936312d32d2b90b17b8eaf4277b
02047788a <jimmy.wu@daedalus.com.tw>
2021-05-12 12:23:45 +0800

    更新執行腳本

--- scripts/start_pool.sh
+++ scripts/start_pool.sh
@@ -1,9 +1,3 @@
 #!/bin/bash
-
-process=$(ps aux | grep "[o]pen-ethereum-pool config.json")
-
-if [ -z "$process" ]
-then
-        cd /media/data/open-ethereum-pool
-        ./build/bin/open-ethereum-pool config.json
-fi
+cd /mnt/ethpool/open-ethereum-pool
+screen ./build/bin/open-ethereum-pool config.json
